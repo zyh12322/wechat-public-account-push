01ef87f93def43359cb29a5477a50bce177660ce
zyh12322 <118092447+zyh12322@users.noreply.github.com>
2022-11-13 13:38:57 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'oIg755x2GZzCY-0d7kgRJpLNZMwQ',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oIg755x2GZzCY-0d7kgRJpLNZMwQ',
     }
   ],
 
